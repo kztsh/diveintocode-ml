b8f9686fca5ab9d5db251119539bdc4c9474a494
arimoto <kztsh.a@gmail.com>
2019-10-30 17:44:34 +0900

    resources deleted

--- resources/access_colab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `seq 0 12`
-do
-  echo "[$i]" ` date '+%y/%m/%d %H:%M:%S'` "connected."
-  open https://colab.research.google.com/drive/1Ss2V4AO_SakK01xmkj_Lvt23XSqbvbRf#scrollTo=2Nr5qmjLmI-R
-  sleep 3600
-done
